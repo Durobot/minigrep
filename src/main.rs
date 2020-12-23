05e89646679bd98479ca6150317935bcab0587fc
Durobot <durobotdev@gmail.com>
2020-12-23 21:10:12 +0200

    Changed parse_config() into Config::new() associated function

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::fs;
 fn main()
 {
     let args: Vec<String> = env::args().collect();
-    let config = parse_config(&args);
+    let config = Config::new(&args);
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
@@ -19,18 +19,21 @@ struct Config<'a>
     filename: &'a str, //String,
 }
 
-// https://stackoverflow.com/questions/30794235/what-is-the-difference-between-a-slice-and-an-array
-//
-// [T; n]  is an array of length n, represented as n adjacent T instances.
-// &[T; n] is purely a reference to that array, represented as a thin pointer to the data.
-// [T]     is a slice, an unsized type; it can only be used through some form of indirection.
-// &[T],   called a slice, is a sized type.
-//         It's a fat pointer, represented as a pointer to the first item and the length of the slice.
-// Arrays thus have their length known at compile time while slice lengths are a runtime matter.
-fn parse_config(args: &[String]) -> Config
+impl<'a> Config<'a>
 {
-    let query = args[1].as_str(); //args[1].clone(); to get a copy of this String
-    let filename = args[2].as_str(); //args[2].clone(); to get a copy of this String
+    // https://stackoverflow.com/questions/30794235/what-is-the-difference-between-a-slice-and-an-array
+    //
+    // [T; n]  is an array of length n, represented as n adjacent T instances.
+    // &[T; n] is purely a reference to that array, represented as a thin pointer to the data.
+    // [T]     is a slice, an unsized type; it can only be used through some form of indirection.
+    // &[T],   called a slice, is a sized type.
+    //         It's a fat pointer, represented as a pointer to the first item and the length of the slice.
+    // Arrays thus have their length known at compile time while slice lengths are a runtime matter.
+    fn new(args: &[String]) -> Config
+    {
+        let query = args[1].as_str(); //args[1].clone(); to get a copy of this String
+        let filename = args[2].as_str(); //args[2].clone(); to get a copy of this String
 
-    Config { query, filename }
+        Config { query, filename }
+    }
 }
