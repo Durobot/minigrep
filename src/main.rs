9d1a9707741e90cb6d4836afefc5c20380567d3f
Durobot <durobotdev@gmail.com>
2020-12-23 23:49:41 +0200

    Instead of panic! Config::new() now returns Result

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main()
 {
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err_str|
+    {
+        println!("Problem parsing arguments: {}", err_str);
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
@@ -29,14 +34,14 @@ impl<'a> Config<'a>
     // &[T],   called a slice, is a sized type.
     //         It's a fat pointer, represented as a pointer to the first item and the length of the slice.
     // Arrays thus have their length known at compile time while slice lengths are a runtime matter.
-    fn new(args: &[String]) -> Config
+    fn new(args: &[String]) -> Result<Config, &'static str>
     {
         if args.len() < 3
-        { panic!("Not enough command line arguments!"); }
+        { return Err("Not enough command line arguments!"); }
 
         let query = args[1].as_str(); //args[1].clone(); to get a copy of this String
         let filename = args[2].as_str(); //args[2].clone(); to get a copy of this String
 
-        Config { query, filename }
+        Ok(Config { query, filename })
     }
 }
