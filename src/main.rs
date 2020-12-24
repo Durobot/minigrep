8154f55c64b7781d422e410d29f5ff14c80ccd57
Durobot <durobotdev@gmail.com>
2020-12-24 16:54:01 +0200

    Switched from println!(), which prints to stdout to eprintln!(), which prints to stderr (doesn't get redirected with > , error messages are printed out on the screen) for error messages

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ fn main()
     let args: Vec<String> = env::args().collect();
     let config = Config::new(&args).unwrap_or_else(|err_str|
     {
-        println!("Problem parsing arguments: {}", err_str);
+        eprintln!("Problem parsing arguments: {}", err_str);
         process::exit(1);
     });
 
@@ -17,7 +17,7 @@ fn main()
     // assign it to e, then execute the {} block
     if let Err(e) = minigrep::run(config) // don't have to bring fns from the lib crate into scope
     {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
     // Since run() returns () within Result::Ok, we don't
